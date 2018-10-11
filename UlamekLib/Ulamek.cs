d27215a882e30815ed425a7b5243d1d9ea9382c4
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2018-10-11 15:39:03 +0200

    Po wykładzie w1s

---
+++ UlamekLib/Ulamek.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UlamekLib
+{
+    public class Ulamek
+    {
+        private readonly long licznik = 0;
+        private readonly long mianownik = 1;
+
+        public long Licznik => licznik;
+        /*
+        {
+            get
+            {
+                return licznik;
+            }
+        }
+        */
+        public long Mianownik => mianownik;
+
+        public Ulamek(long licznik, long mianownik )
+        {
+            this.licznik = licznik;
+            if (mianownik == 0)
+                throw new DivideByZeroException("mianownik nie może być 0");
+
+            this.mianownik = mianownik;
+        }
+
+        public Ulamek() : this(0 ,1) { }
+
+        public Ulamek( long liczba ) : this(liczba, 1) { }
+
+        public Ulamek( string napis )
+        {
+            //to do
+            throw new NotImplementedException();
+        }
+
+        public Ulamek( double )
+        {
+            //to do
+            throw new NotImplementedException();
+        }
+
+        public override string ToString() //=> $"{licznik}/{mianownik}";
+        {
+            return $"{licznik}/{mianownik}";
+        }
+
+    }
+}
--- UlamekTest/Program.cs
+++ UlamekTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UlamekLib;
 
 namespace UlamekTest
 {
@@ -10,6 +11,15 @@ namespace UlamekTest
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine(0.1 + 0.2 == 0.3);
+            
+            Ulamek u = new Ulamek();
+            Console.WriteLine(u);
+            Ulamek v = new Ulamek(2, 10);
+            Console.WriteLine(v);
+            Ulamek w = new Ulamek(3, 10);
+            //Console.WriteLine(u + v == w);
+            
         }
     }
 }
